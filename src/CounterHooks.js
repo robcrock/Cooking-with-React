86a06b55ddf2558aebadbec276d4c72a882acd51
robcrockPI <robert@propellerindustries.com>
2020-11-14 10:55:47 -0300

    Use Context to create a theme and passes the theme through both a class and funtional component.

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { ThemeContext } from './App'
 
 export default function CounerHooks({ initialCount }) {
   const [count, setCount] = useState(initialCount)
+  const style = useContext(ThemeContext)
 
   return (
     <div>
-      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button style={ style } onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
       <span>{ count }</span>
-      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      <button style={ style } onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
     </div>
   )
 }
