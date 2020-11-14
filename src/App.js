4b76fd99d44463bad11c18ad8d3fd345d4de425f
robcrockPI <robert@propellerindustries.com>
2020-11-14 08:11:47 -0300

    Created a Counter as Class Component and a Functional Component.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Counter from './Counter'
+import CounterHooks from './CounterHooks'
+
+function App() {
+  return (
+    <>
+      Counter
+      <Counter initialCount={0}/>
+      Counter Hooks
+      <CounterHooks initialCount={0}/>
+    </>
+  )
+}
+
+export default App;
