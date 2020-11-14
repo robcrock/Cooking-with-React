d555deb3a717f4268edb6926ad6dd7cc7a21cb9d
robcrockPI <robert@propellerindustries.com>
2020-11-14 13:16:39 -0300

    Added a section to the recipes via an ingredients list.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ const sampleRecipes = [
     cooktime: "1:45",
     instructions:
       "1. Put salt on chicken 2. Put chicken in oven 3. Eat chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs'
+      }
+    ]
   },
   {
     id: 2,
@@ -24,6 +36,18 @@ const sampleRecipes = [
     servings: 5,
     cooktime: "0:45",
     instructions: "1. Put paprika on chicken 2. Put pork in oven 3. Eat pork",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs'
+      }
+    ]
   },
 ];
 
